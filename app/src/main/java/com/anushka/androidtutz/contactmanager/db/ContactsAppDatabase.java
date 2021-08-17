60d73c4bb16d1d6d86fcda31a870c815c6865cf5
Paul Wang <paul.wang.is.007@gmail.com>
2021-08-16 23:13:21 -0700

    Use Java code make project compile.

---
+++ app/src/main/java/com/anushka/androidtutz/contactmanager/db/ContactsAppDatabase.java
@@ -0,0 +1,10 @@
+package com.anushka.androidtutz.contactmanager.db;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import com.anushka.androidtutz.contactmanager.db.entity.Contact;
+
+@Database(entities = {Contact.class}, version = 1, exportSchema = false)
+public abstract class ContactsAppDatabase extends RoomDatabase{
+    public abstract ContactDAO getContactDAO();
+}
